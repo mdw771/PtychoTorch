FIX: fix gradient scaling

ptytorch/forward_models.py
@@ -39,7 +39,7 @@ class Ptychography2DForwardModel(ForwardModel):
                 # self.register_parameter(name=var.name, param=torch.nn.Parameter(var.tensor))
                 self.optimizable_variables.append(var)
 
-    def forward(self, positions: Tensor) -> Tensor:
+    def forward(self, positions: Tensor, return_object_patches: bool = False) -> Tensor:
         """Run ptychographic forward simulation and calculate the measured intensities.
 
         :param patterns: A (N, H, W) tensor of diffraction patterns in the batch.
@@ -51,8 +51,19 @@ class Ptychography2DForwardModel(ForwardModel):
         for i_probe_mode in range(self.probe.n_modes):
             p = self.probe.get_mode(i_probe_mode)
             psi = obj_patches * p
-            psi_far = torch.fft.fft2(psi)
+            psi_far = torch.fft.fft2(psi, norm='ortho')
             psi_far = torch.fft.fftshift(psi_far, dim=(-2, -1))
             y = y + torch.abs(psi_far) ** 2
-        return y
-                
+            
+        # Revert the 1 / n factor due to batch-averaged loss
+        # TODO: theoretically the gradient should be multiplied by |p|^2 / max(|p|^2) to be
+        # equal to ePIE update function, but it doesn't seem to be the case. Need to investigate.
+        psi.register_hook(lambda grad: grad * float(obj_patches.shape[0]))
+        
+        returns = [y]
+        if return_object_patches:
+            returns.append(obj_patches)
+        if len(returns) == 1:
+            return returns[0]
+        else:
+            return returns

ptytorch/metrics.py
@@ -1,12 +1,22 @@
+from typing import Optional
+
 import torch
 from torch import Tensor
 
 
 class MSELossOfSqrt(torch.nn.MSELoss):
     
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, eps=1e-7, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.eps = eps
     
-    def forward(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
-        l = super().forward(torch.sqrt(y_pred), torch.sqrt(y_true))
+    def forward(self, y_pred: Tensor, y_true: Tensor, weight_map: Optional[Tensor] = None) -> Tensor:
+        if weight_map is None:
+            l = super().forward(torch.sqrt(y_pred + self.eps), torch.sqrt(y_true + self.eps))
+        else:
+            d = ((torch.sqrt(y_pred + self.eps) - torch.sqrt(y_true + self.eps)) ** 2) * weight_map
+            if self.reduction == 'mean':
+                l = torch.mean(d)
+            elif self.reduction == 'sum':
+                l = torch.sum(d)
         return l

ptytorch/reconstructors.py
@@ -100,7 +100,7 @@ class AutodiffReconstructor(IterativeReconstructor):
             for batch_data in self.dataloader:
                 input_data = [x.to(torch.get_default_device()) for x in batch_data[:-1]]
                 y_true = batch_data[-1].to(torch.get_default_device())
-                                
+                
                 y_pred = self.forward_model(*input_data)
                 batch_loss = self.loss_function(y_pred, y_true)
 
@@ -180,11 +180,12 @@ class EPIEReconstructor(IterativeReconstructor):
         psi_prime = psi_far / torch.abs(psi_far) * torch.sqrt(y_true + 1e-7)
         psi_prime = torch.fft.ifft2(torch.fft.ifftshift(psi_prime, dim=(-2, -1)))
         
+        # Update object
         delta_o = self.alpha * p.conj() / (torch.abs(p) ** 2).max()
         delta_o = delta_o * (psi_prime - psi)
-        
         self.get_forward_model().object.place_patches(positions, delta_o, op='add')
         
+        # Update probe
         delta_p = self.alpha * obj_patches.conj() / (torch.abs(obj_patches) ** 2).max(-1).values.max(-1).values.view(-1, 1, 1)
         delta_p = delta_p * (psi_prime - psi)
         delta_p = delta_p.mean(0)

ptytorch/utils.py
@@ -9,15 +9,15 @@ from numpy import ndarray
 default_complex_dtype = torch.complex64
 
 
-def get_suggested_object_size(positions_px, probe_shape):
-    h = np.ceil(positions_px[:, 0].max() - positions_px[:, 0].min()) + probe_shape[0]
-    w = np.ceil(positions_px[:, 1].max() - positions_px[:, 1].min()) + probe_shape[1]
+def get_suggested_object_size(positions_px, probe_shape, extra=0):
+    h = np.ceil(positions_px[:, 0].max() - positions_px[:, 0].min()) + probe_shape[0] + extra
+    w = np.ceil(positions_px[:, 1].max() - positions_px[:, 1].min()) + probe_shape[1] + extra
     return (int(h), int(w))
 
 
 def rescale_probe(probe: ndarray, patterns: ndarray):
     i_data = np.sum(np.mean(patterns, axis=0))
-    i_probe = np.sum(np.array([np.abs(np.fft.fft2(p)) ** 2 for p in probe]))
+    i_probe = np.sum(np.array([np.abs(np.fft.fft2(p, norm='ortho')) ** 2 for p in probe]))
     factor = i_data / i_probe
     probe = probe * np.sqrt(factor)
     return probe

workspace/tungsten/reconstruct.py
@@ -1,14 +1,17 @@
+import datetime
+
 import torch
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+import tifffile
 
 from ptytorch.data_structures import *
 from ptytorch.io_handles import PtychographyDataset
 from ptytorch.forward_models import Ptychography2DForwardModel
 from ptytorch.utils import (get_suggested_object_size, set_default_complex_dtype, get_default_complex_dtype, 
                             rescale_probe)
-from ptytorch.reconstructors import AutodiffReconstructor
+from ptytorch.reconstructors import *
 from ptytorch.metrics import MSELossOfSqrt
 
 
@@ -16,6 +19,11 @@ patterns = h5py.File('data/dp_250.hdf5', 'r')['dp'][...]
 
 f_meta = h5py.File('data/metadata_250_truePos.hdf5', 'r')
 probe = f_meta['probe'][...]
+
+#####
+# probe = probe[0:1]
+#####
+
 probe = rescale_probe(probe, patterns)
 positions = np.stack([f_meta['probe_position_y_m'][...], f_meta['probe_position_x_m'][...]], axis=1)
 pixel_size_m = 8e-9
@@ -23,17 +31,18 @@ pixel_size_m = 8e-9
 positions_px = positions / pixel_size_m
 dataset = PtychographyDataset(patterns, positions_px)
 object = Object2D(
-    data=torch.ones(get_suggested_object_size(positions_px, probe.shape[-2:]), dtype=get_default_complex_dtype()), 
+    data=torch.ones(get_suggested_object_size(positions_px, probe.shape[-2:], extra=100), dtype=get_default_complex_dtype()), 
     pixel_size_m=pixel_size_m,
     optimizable=True,
-    optimizer_class=torch.optim.Adam,
-    optimizer_params={'lr': 1e-3}
+    optimizer_class=torch.optim.SGD,
+    optimizer_params={'lr': 1e-1}
 )
+
 probe = Probe(
     data=probe,
-    optimizable=False,
-    optimizer_class=torch.optim.Adam,
-    optimizer_params={'lr': 1e-6}
+    optimizable=True,
+    optimizer_class=torch.optim.SGD,
+    optimizer_params={'lr': 1e0}
 )
 probe_positions = ProbePositions(
     data=positions_px,
@@ -59,9 +68,21 @@ reconstructor = AutodiffReconstructor(
     loss_function=MSELossOfSqrt(),
     n_epochs=64
 )
+# reconstructor = EPIEReconstructor(
+#     dataset=dataset,
+#     forward_model=forward_model,
+#     batch_size=96,
+#     n_epochs=64,
+#     alpha=1e-1
+# )
 reconstructor.build()
 reconstructor.run()
 
-recon = reconstructor.get_forward_model().object.tensor.phase().detach().cpu().numpy()
-plt.imshow(recon)
-plt.show()
+recon = reconstructor.get_forward_model().object.tensor.complex().detach().cpu().numpy()
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(np.angle(recon)); ax[0].set_title('Phase')
+ax[1].imshow(np.abs(recon)); ax[1].set_title('Magnitude')
+timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+plt.savefig('outputs/recon_{}.png'.format(timestamp))
+tifffile.imwrite('outputs/recon_phase_{}.tif'.format(timestamp), np.angle(recon))
+tifffile.imwrite('outputs/recon_mag_{}.tif'.format(timestamp), np.abs(recon))